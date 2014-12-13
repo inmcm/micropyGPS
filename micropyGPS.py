63fead0b40998e60386c6564df77f104fa862d33
inmcm <calvin.mccoy@gmail.com>
2014-12-13 12:57:45 -0500

    Remove non-ASCII characters and some PEP8 formatting

--- micropyGPS.py
+++ micropyGPS.py
@@ -6,14 +6,14 @@
 # Copyright (c) 2014 Michael Calvin McCoy (calvin.mccoy@gmail.com)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
 # rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
 # persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
 # Software.
 #
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
 # WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -401,7 +401,7 @@ class MicropyGPS(object):
                     self.crc_xor ^= ascii_char
 
                 # If a Valid Sentence Was received and it's a supported sentence, then parse it!!
-                if valid_sentence :
+                if valid_sentence:
 
                     # Clear Active Processing Flag
                     self.sentence_active = False
@@ -416,7 +416,6 @@ class MicropyGPS(object):
                 # Check that the sentence buffer isn't filling up with Garage waiting for the sentence to complete
                 if self.char_count > self.SENTENCE_LIMIT:
                     self.sentence_active = False
-
 
         # Tell Host no new sentence was parsed
         return None
@@ -445,9 +444,6 @@ class MicropyGPS(object):
 
     # All the currently supported NMEA sentences    
     supported_sentences = {'GPRMC': gprmc, 'GPGGA': gpgga, 'GPVTG': gpvtg, 'GPGSA': gpgsa, 'GPGSV': gpgsv}
-
-
-
 
 if __name__ == "__main__":
 
